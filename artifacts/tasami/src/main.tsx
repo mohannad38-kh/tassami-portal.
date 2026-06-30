a108920deebbe93354148dc61c5d091f23cda112
mohannad38-kh <mohannadkhaloof38@gmail.com>
2026-06-30 12:00:13 +0000

    connect frontend to render backend

--- artifacts/tasami/src/main.tsx
+++ artifacts/tasami/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
+
+setBaseUrl("https://tassami-portal-1.onrender.com");
 
 createRoot(document.getElementById("root")!).render(<App />);
