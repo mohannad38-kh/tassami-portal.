766a4d7248c81535a72c020b49256e25d8d951f6
mohannad38-kh <mohannadkhaloof38@gmail.com>
2026-07-01 11:39:05 +0000

    update backend url

--- artifacts/tasami/src/main.tsx
+++ artifacts/tasami/src/main.tsx
@@ -3,7 +3,7 @@ import { setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
 
-setBaseUrl("https://tassami-portal-1.onrender.com");
+setBaseUrl("https://tassami-portal-ff5j.onrender.com");
 
 createRoot(document.getElementById("root")!).render(<App />);
 // trigger Tue Jun 30 12:04:21 PM UTC 2026
